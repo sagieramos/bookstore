19355a55d8f1f851b5704bf6a3dc4b0fd7435a9f
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-25 20:00:36 +0100

    create reducer functions

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: { books: [] },
+  reducers: {
+    addBooks: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book !== action.payload.id);
+    },
+  },
+});
+
+export const { addBooks, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
