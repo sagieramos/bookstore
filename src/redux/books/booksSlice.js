fd46d444e02a439ee6e0a3e7fcc0f9342853e799
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-25 21:36:01 +0100

    initialState is assigned an empty array

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState: { books: [] },
+  initialState: [],
   reducers: {
     addBooks: (state, action) => {
       state.books.push(action.payload);
