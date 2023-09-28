e68d42426e23f6f2c7db6e502de3593b73dca245
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-28 01:42:31 +0100

    Remove thunk installed

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import BookForm from './BookForm';
+import { fetchBooks } from '../redux/books/booksSlice';
 import './BookList.scss';
 
 const BookList = () => {
-  const books = useSelector((store) => store.book);
+  const books = useSelector((store) => store.book.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
     <div>
       <div className="book-list">
         {books.map((book) => (
-          <Book key={book.item_id} book={book} />
+          <Book key={book.itemId} book={book} />
         ))}
       </div>
       <div>
